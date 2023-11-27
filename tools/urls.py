1573542bcfcdcd8550c2af282b1f0d761856abe2
levinyi <dushiyi319@163.com>
2023-11-27 12:47:06 +0800

    some changes

--- tools/urls.py
+++ tools/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('index/', views.index, name="index"),
     path('tools-list/', views.tools_list, name="tools_list"),
     path('tools-use/<str:tools_id>/', views.tools_use, name='tools_use'),
-    path('check-status/<str:tools_id>', views.check_status, name='check_status'),
-    path('download_result/<str:unique_id>', views.download_result, name='download_result'),
-    path('delete_status/<str:unique_id>', views.delete_status, name='delete_status'),
-    path('test/', views.test, name="test")
+    path('check_status/<str:tools_id>/', views.check_status, name='check_status'),
+    path('download_result/<str:unique_id>/', views.download_result, name='download_result'),
+    path('delete_status/<str:unique_id>/', views.delete_status, name='delete_status'),
+    path('test/', views.test, name="test"),
+    path('update_data_path/', views.update_data_path, name='update_data_path'),
 ]
