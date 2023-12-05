adc3c6ee13464840fe2dad19d8491f6e3654a33b
levin <dushiyi319@163.com>
2023-12-05 10:40:18 +0800

    some changes

--- tools/scripts/hpa.summary.report.py
+++ tools/scripts/hpa.summary.report.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import os
-import sys
-from collections import defaultdict
 import argparse
 
 def deal_hpa_db(hpa_db):
@@ -10,16 +8,19 @@ def deal_hpa_db(hpa_db):
     return db_dict
 
 def main(project_dir, hpa_db, outputfile):
-    # script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    # hpa_db = os.path.join(script_dir, 'database',"HPA.Gene.cDNA_Changes.xls")
-
     hpa_db_dict = deal_hpa_db(hpa_db)
     file_list = [i.rstrip("\n") for i in os.popen(f"find {project_dir} -type f -name *brief.table.xls").readlines()]
     print(f"find {len(file_list)} samples in {project_dir} directory")
 
     header= ["Sample"] + [f"HPA-{i}"  for i in range(1, 36)]
     big_list = [header]
-
+    '''
+        判断方法：
+        1. 当样本中的 cDNA_Changes 存在时，检查 homo/het 列的值。
+        2. 如果值是 homo，则应该使用 df_hpa 中对应的 het 列的值。
+        3. 如果值是 het，则应该将 df_hpa 中的 homo 和 het 列的值结合起来。
+        4. 如果样本中没有检测到 df_hpa 数据库中的 cDNA_Changes，则使用 df_hpa 中的 homo 列的值。
+    '''
     for each in file_list:
         content = []
         sample_name = os.path.basename(os.path.dirname(each))
@@ -29,17 +30,24 @@ def main(project_dir, hpa_db, outputfile):
         df_hpa = df[df["System"] == "HPA"]
         # what if df_hpa is empty?
         
-        cdna_changes = df_hpa['cDNA_Changes'].values.tolist()
+        # cdna_changes = df_hpa['cDNA_Changes'].values.tolist()
 
         for cdna_change, homo_het in hpa_db_dict.items():
-            if cdna_change in cdna_changes:
+            if cdna_change in df_hpa['cDNA_Changes'].values:
                 df_filtered = df_hpa[(df_hpa['cDNA_Changes'] == cdna_change)]
-                if (df_filtered['homo/het'] == 'homo').any():
-                    content.append(homo_het['homo'])
-                elif (df_filtered['homo/het'] == 'het').any():
-                    content.append(homo_het['het'])
+                homo_het_value = df_filtered.iloc[0]['homo/het']  # 获取homo/het列的值
+                if homo_het_value == 'homo':
+                    # 如果是homo，使用het列的值
+                    content.append(str(homo_het['het']))
+                else:
+                    # 如果是het，将homo和het的值结合起来
+                    content.append(f"{homo_het['homo']},{homo_het['het']}")
+                # if (df_filtered['homo/het'] == 'homo').any():
+                #     content.append(homo_het['homo'])
+                # elif (df_filtered['homo/het'] == 'het').any():
+                #     content.append(homo_het['het'])
             else:
-                content.append(f"{homo_het['homo']},{homo_het['het']}")
+                content.append(str(homo_het['homo']))
 
         big_list.append(content)
     big_df = pd.DataFrame(big_list[1:], columns=big_list[0])
