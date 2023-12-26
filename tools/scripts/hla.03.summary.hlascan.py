d354f452f1a2ec1c18f30c86c377cd00d9b00716
levin <dushiyi319@163.com>
2023-12-26 10:49:44 +0800

    some chagnes

--- tools/scripts/hla.03.summary.hlascan.py
+++ tools/scripts/hla.03.summary.hlascan.py
@@ -11,14 +11,17 @@ print("find {} samples in current directory".format(len(file_list)))
 
 big_df = pd.DataFrame()
 for each in file_list:
+    print(each)
     df = pd.read_csv(each, sep="\t")
     big_df = pd.concat([big_df, df], axis=1)
 
 # 转置big_df
 big_df = big_df.T
+print(big_df)
 # 重置索引
 big_df = big_df.reset_index()
 # 重命名列名
+print(big_df)
 big_df.to_csv(os.path.join(result_path, "HLAscan_Result_final.summary.xls"), index=False, header=True)
 
 print("Output file: HLAscan_Result_final.summary.xls")
